44e83253f54bcc2d04a7bf97ac38200a74da4759
Homo-not-so-sapient <Homo-not-so-sapient@LAPTOP-9KLS1JQT>
2021-04-20 12:24:23 +0530

    crud updated

--- project/src/main/java/com/ResearcherService.java
+++ project/src/main/java/com/ResearcherService.java
@@ -1,5 +1,26 @@
 package com;
+import model.Researcher; 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
 
-public class ResearcherService {
+//For JSON
+import com.google.gson.*; 
 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Researcher") 
+public class ResearcherService 
+{ 
+ Researcher itemObj = new Researcher(); 
+@GET
+@Path("/") 
+@Produces(MediaType.TEXT_HTML) 
+public String readItems() 
+ { 
+ return "done!"; 
+ } 
 }
--- project/src/main/java/model/Researcher.java
+++ project/src/main/java/model/Researcher.java
@@ -10,13 +10,13 @@ private Connection connect()
  Class.forName("com.mysql.jdbc.Driver"); 
  
  //Provide the correct details: DBServer/DBName, username, password 
- con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", ""); 
+ con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Local instance MySQL80", "root", ""); 
  } 
  catch (Exception e) 
  {e.printStackTrace();} 
  return con; 
  } 
-public String insertItem(String code, String name, String price, String desc) 
+public String insertItem(String code, String name,String project, String price, String desc) 
  { 
  String output = ""; 
  try
@@ -24,16 +24,21 @@ public String insertItem(String code, String name, String price, String desc)
  Connection con = connect(); 
  if (con == null) 
  {return "Error while connecting to the database for inserting."; } 
+ 
+ 
  // create a prepared statement
- String query = " insert into researchdb (`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)"
- + " values (?, ?, ?, ?, ?)"; 
+ String query = " insert into researcher (`r_id`,`r_code`, `r_name`,`r_project`,`r_project_price`,`r_project_desc`)"
+ + " values (1,abc, abc, abc,190,abc)"; 
  PreparedStatement preparedStmt = con.prepareStatement(query); 
+ 
  // binding values
  preparedStmt.setInt(1, 0); 
  preparedStmt.setString(2, code); 
  preparedStmt.setString(3, name); 
- preparedStmt.setDouble(4, Double.parseDouble(price)); 
- preparedStmt.setString(5, desc); 
+ preparedStmt.setString(4, project); 
+ preparedStmt.setDouble(5, Double.parseDouble(price)); 
+ preparedStmt.setString(6, desc); 
+ 
 // execute the statement
  preparedStmt.execute(); 
  con.close(); 
@@ -57,38 +62,37 @@ public String readItems()
  {return "Error while connecting to the database for reading."; } 
  
  // Prepare the html table to be displayed
- output = "<table border='1'><tr><th>Item Code</th><th>Item Name</th>" +
- "<th>Item Price</th>" + 
- "<th>Item Description</th>" +
+ output = "<table border='1'><tr><th>Researcher Code</th><th>Researcher Name</th>" +
+ "<th>Project</th>" + 
+ "<th>Project Description</th>" +
+ "<th>Project price</th>" +
  "<th>Update</th><th>Remove</th></tr>"; 
  
- String query = "select * from items"; 
+ String query = "select * from researcher"; 
  Statement stmt = con.createStatement(); 
  ResultSet rs = stmt.executeQuery(query); 
+ 
  // iterate through the rows in the result set
  while (rs.next()) 
  { 
- String itemID = Integer.toString(rs.getInt("itemID")); 
- String itemCode = rs.getString("itemCode"); 
- String itemName = rs.getString("itemName"); 
- String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
- String itemDesc = rs.getString("itemDesc"); 
- // Add into the html table
- output += "<tr><td>" + itemCode + "</td>"; 
- output += "<td>" + itemName + "</td>"; 
- output += "<td>" + itemPrice + "</td>"; 
- output += "<td>" + itemDesc + "</td>"; 
+ String r_id = Integer.toString(rs.getInt("r_id")); 
+ String r_code = rs.getString("r_code"); 
+ String r_project = rs.getString("r_project"); 
+ String r_project_price = Double.toString(rs.getDouble("r_project_price")); 
+ String r_project_desc = rs.getString("r_project_desc"); 
+ 
+ // Add into the HTML table
+ output += "<tr><td>" + r_code + "</td>"; 
+ output += "<td>" + r_project + "</td>"; 
+ output += "<td>" + r_project_price + "</td>"; 
+ output += "<td>" + r_project_desc + "</td>"; 
  
  // buttons
- output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
- + "<td><form method='post' action='items.jsp'>"
- + "<input name='btnRemove' type='submit' value='Remove' 
- class='btn btn-danger'>"
- + "<input name='itemID' type='hidden' value='" + itemID 
- + "'>" + "</form></td></tr>"; 
+ output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" + "<td><form method='post' action='researcher.jsp'>" + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" + "<input name='r_id' type='hidden' value='" + r_id  + "'>" + "</form></td></tr>"; 
  } 
  con.close(); 
- // Complete the html table
+ 
+ // Complete the HTML table
  output += "</table>"; 
  } 
  catch (Exception e) 
@@ -98,7 +102,7 @@ public String readItems()
  } 
  return output; 
  } 
-public String updateItem(String ID, String code, String name, String price, String desc)
+public String updateItem(String ID, String code, String name,String project, String price, String desc)
 { 
 	 String output = ""; 
 	 try
@@ -107,15 +111,16 @@ public String updateItem(String ID, String code, String name, String price, Stri
 	 if (con == null) 
 	 {return "Error while connecting to the database for updating."; } 
 	 // create a prepared statement
-	 String query = "UPDATE items SET itemCode=?,itemName=?,itemPrice=?,itemDesc=? WHERE itemID=?"; 
+	 String query = "UPDATE researcher SET r_code=?,r_name=?,r_project=?,r_project_price=?,r_project_desc=? WHERE r_id=?"; 
 	 PreparedStatement preparedStmt = con.prepareStatement(query); 
 	 
 	 // binding values
 	 preparedStmt.setString(1, code); 
 	 preparedStmt.setString(2, name); 
-	 preparedStmt.setDouble(3, Double.parseDouble(price)); 
-	 preparedStmt.setString(4, desc); 
-	 preparedStmt.setInt(5, Integer.parseInt(ID)); 
+	 preparedStmt.setString(3, project); 
+	 preparedStmt.setDouble(4, Double.parseDouble(price)); 
+	 preparedStmt.setString(5, desc); 
+	 
 	 // execute the statement
 	 preparedStmt.execute(); 
 	 con.close(); 
@@ -123,12 +128,12 @@ public String updateItem(String ID, String code, String name, String price, Stri
 	 } 
 	 catch (Exception e) 
 	 { 
-	 output = "Error while updating the item."; 
+	 output = "Error while updating researcher details."; 
 	 System.err.println(e.getMessage()); 
 	 } 
 	 return output; 
 	 } 
-	public String deleteItem(String itemID) 
+	public String deleteItem(String r_id) 
 	 { 
 	 String output = ""; 
 	 try
@@ -136,11 +141,14 @@ public String updateItem(String ID, String code, String name, String price, Stri
 	 Connection con = connect(); 
 	 if (con == null) 
 	 {return "Error while connecting to the database for deleting."; } 
+	 
 	 // create a prepared statement
-	 String query = "delete from items where itemID=?"; 
+	 String query = "delete from researcher where r_id=?"; 
 	 PreparedStatement preparedStmt = con.prepareStatement(query); 
+	 
 	 // binding values
-	 preparedStmt.setInt(1, Integer.parseInt(itemID)); 
+	 preparedStmt.setInt(1, Integer.parseInt(r_id)); 
+	 
 	 // execute the statement
 	 preparedStmt.execute(); 
 	 con.close(); 
@@ -148,7 +156,7 @@ public String updateItem(String ID, String code, String name, String price, Stri
 	 } 
 	 catch (Exception e) 
 	 { 
-	 output = "Error while deleting the item."; 
+	 output = "Error while deleting the researcher."; 
 	 System.err.println(e.getMessage()); 
 	 } 
 	 return output; 
